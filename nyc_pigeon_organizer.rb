c058b02e46ba38b68904367bd752c2cde1122782
Peter Sampson <petersampson010@gmail.com>
2019-09-19 13:18:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,7 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |section, section_hash|#3times
-    section_hash.each do |sub_section, names_array|
-      names_array.each do |name|
-        new_hash[name.to_s][section] = sub_section
-      end 
-    end 
-  end
-  return new_hash
+  data[:gender].each do |m_f, m_f_array|
+    m_f_array.each do |
 end 
 
 

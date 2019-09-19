d08b8cb488ec4984aff14097520f814b644d75b3
Peter Sampson <petersampson010@gmail.com>
2019-09-19 13:49:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,8 @@ def nyc_pigeon_organizer(data)
     end 
   end 
   data[:location].each do |l, l_array|
-    l_array.each do |name|
-      new_hash[name][:lives] = l.to_s
+    l_array.each do |name1|
+      new_hash[name1][:lives] = l.to_s
     end 
   end 
   return new_hash

f898900088765145eb1d2cffe147ba5d3c971275
Peter Sampson <petersampson010@gmail.com>
2019-09-19 12:37:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each do |section, section_hash|#3times
     section_hash.each do |sub_section, names_array|
       names_array.each do |name|
-        if new_hash.include?
+        if new_hash.include?(name)
+          new_hash[name] << {
           
   
         end 

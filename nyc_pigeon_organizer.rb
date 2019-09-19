8df222208d2882873c93f9d35091b1e1700b13f8
Peter Sampson <petersampson010@gmail.com>
2019-09-19 14:01:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  new_hash = {}
+  data[:gender].each do |m_f, m_f_array|
+    m_f_array.each do |name|
+      new_hash[name] = {:color => [], :gender => [m_f.to_s], :lives => []}
+    end 
+  end 
+  data[:lives].each do |l, l_array|
+    l_array.each do |name|
+      new_hash[name][:lives] << l.to_s
+    end 
+  end 
+  data[:color].each do |c, c_array|
+    c_array.each do |name|
+      new_hash[name][:color] << c.to_s
+    end 
+  end 
+  return new_hash
+end 
+
+

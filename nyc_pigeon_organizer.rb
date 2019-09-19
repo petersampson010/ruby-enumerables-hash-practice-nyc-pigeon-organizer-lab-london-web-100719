d5378f383e5396f8972cae0fdddfd17465858e35
Peter Sampson <petersampson010@gmail.com>
2019-09-19 10:15:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,6 @@ def nyc_pigeon_organizer(data)
   data.each do |item, other|
     other.each do |section, array|
       array.each do |name|
-        new_hash[name] = {:color 
+        new_hash[name] = {:color => [], :gender => [], lives => []}
 end 
 
